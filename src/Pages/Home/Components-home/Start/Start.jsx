6bd9173ebe14111c81c2e01b3b5fab612c3faea5
yagovysk <yago09regis@gmail.com>
2024-07-29 17:17:08 -0300

    add

--- src/Pages/Home/Components-home/Start/Start.jsx
+++ src/Pages/Home/Components-home/Start/Start.jsx
@@ -4,7 +4,7 @@ export function Start() {
   return (
     <section className="section-start">
       <div className="start-button">
-        <h1>Transformando Bem-Estar e Beleza em Realidade!</h1>
+        <h1>Sua Saúde com conforto!</h1>
         <a
           href="https://wa.me/556130201781?text=Olá, vim pelo site, gostaria marcar uma consulta."
           target="blank"

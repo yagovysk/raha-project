340738cd8fe1889ac612336c259a0faf41e86a72
yagovysk <yago09regis@gmail.com>
2024-08-22 16:47:34 -0300

    add

--- src/Pages/Home/Components-home/Start/Start.jsx
+++ src/Pages/Home/Components-home/Start/Start.jsx
@@ -1,21 +1,35 @@
 import logo from "../../../../assets/logo-background.png";
+import { Helmet } from "react-helmet";
 import "./Start.css";
 
 export function Start() {
   return (
     <section className="section-start">
+      <Helmet>
+        <title>Clínica Fisioterapia e Estética - Raha Saúde e Estética</title>
+        <meta
+          name="description"
+          content="Descubra os melhores e mais confiáveis serviços de fisioterapia e estética do Distrito Federal na Raha Saúde e Estética."
+        />
+        <meta
+          name="keywords"
+          content="fisioterapia, estética, clínica, saúde, pós-operatório, cirurgia ortognática, DF"
+        />
+        <meta name="author" content="Raha Saúde e Estética" />
+      </Helmet>
       <div className="start-button">
         <img
           className="logo-background"
           src={logo}
-          alt="Logo da raha, um símboloarábe que significa conforto."
+          alt="Logo da Raha Saúde e Estética, representando conforto com um símbolo árabe."
         />
-        <h1>Sua Saúde com conforto começa aqui!</h1>
+        <h1>Sua Saúde com Conforto Começa Aqui!</h1>
         <a
           href="https://wa.me/556130201781?text=Olá, vim pelo site, gostaria marcar uma consulta."
-          target="blank"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          Entre em contato!
+          Entre em Contato!
         </a>
       </div>
     </section>
